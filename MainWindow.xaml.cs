2ffa8287e1952a44ebfe41e1e93f961a62832789
karcag.tamas <karcagtamas@gmail.com>
2019-02-20 22:43:04 +0100

    Sudoku task and complete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace Sudoku
     public class Field
     {
         public int number { get; set; }
+        public bool isTask { get; set; }
     }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -26,24 +27,17 @@ namespace Sudoku
     public partial class MainWindow : Window
     {
         public Field[,] Fields;
-        public Brush[] colors = new Brush[] { Brushes.DarkMagenta, Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.Cyan, Brushes.Magenta, Brushes.Black, Brushes.DarkGreen, Brushes.Orange };
+        public Field[,] Completed;
+        public Brush[] colors = new Brush[] { Brushes.DarkMagenta, Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.DarkViolet, Brushes.Magenta, Brushes.DarkOliveGreen, Brushes.DarkGreen, Brushes.Orange };
         public MainWindow()
         {
             InitializeComponent();
             this.Fields = new Field[9, 9];
-            
-
-            do
-            {
-                this.refresh();
-                this.generateTable();
-            } while (!this.isValid());
-                this.writeOut();
-
         }
 
         public void refresh()
         {
+            
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
@@ -55,17 +49,29 @@ namespace Sudoku
 
         public void writeOut()
         {
+            for (int i = 0; i < GridSudoku.Children.Count - 1; i++)
+            {
+                if (GridSudoku.Children[i].GetType() == typeof(Label))
+                {
+                    Label L = (Label)GridSudoku.Children[i];
+                    GridSudoku.Children.Remove(L);
+                }
+            }
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
                     Label label = new Label();
-                    label.Content = this.Fields[i, j].number;
+                    label.Content = this.Fields[i, j].number != 0 ? this.Fields[i,j].number.ToString() : " ";
                     label.FontSize = 16;
                     label.HorizontalAlignment = HorizontalAlignment.Center;
                     label.VerticalAlignment = VerticalAlignment.Center;
                     if (this.Fields[i, j].number != 0) label.Foreground = this.colors[this.Fields[i, j].number - 1];
-                    else label.Background = Brushes.Red;
+                    label.Background = Brushes.LightGray;
+                    label.Width = 50;
+                    label.Height = 50;
+                    label.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    label.VerticalContentAlignment = VerticalAlignment.Center;
                     label.SetValue(Grid.RowProperty, i);
                     label.SetValue(Grid.ColumnProperty, j);
                     GridSudoku.Children.Add(label);
@@ -88,34 +94,40 @@ namespace Sudoku
                     {
                         x = r.Next(1,10);
                         count++;
-                    } while (check(i,j,x) && count < 100);
-                    if (count < 100) this.Fields[i, j].number = x;
+                    } while (check(i,j,x, this.Fields) && count < 100);
+                    if (count < 100)
+                    {
+                        this.Fields[i, j].number = x;
+                    }
                     else f = false;
                 }
             }
         }
 
-        public bool check(int i, int j, int x)
+        public bool check(int i, int j, int x, Field[,] F)
         {
-            if (checkRow(i, x) && checkCol(j, x) && checkOneBlock(i,j,x)) return false;
+            if (checkRow(i, x, F) && checkCol(j, x, F) && checkOneBlock(i,j,x, F)) return false;
             else return true;
         }
 
-        public bool checkRow(int i, int x){
+        public bool checkRow(int i, int x, Field[,] F)
+        {
             for (int j = 0; j < this.Fields.GetLength(0); j++){
                 if (this.Fields[i,j].number == x) return false;
             }
             return true;
         }
 
-        public bool checkCol(int j, int x){
+        public bool checkCol(int j, int x, Field[,] F)
+        {
             for (int i = 0; i < this.Fields.GetLength(1); i++){
                 if (this.Fields[i,j].number == x) return false;
             }
             return true;
         }
 
-        public bool checkOneBlock(int i, int j, int x){
+        public bool checkOneBlock(int i, int j, int x, Field[,] F)
+        {
             int row = i / 3;
             int col = j / 3;
             int posi = row * 3 + 1;
@@ -132,13 +144,90 @@ namespace Sudoku
             return true;
         }
 
-        public bool isValid()
+        public bool isValid(Field[,] F)
         {
-            foreach (var i in this.Fields)
+            foreach (var i in F)
             {
                 if (i.number == 0) return false;
             }
             return true;
+        }
+
+        public void deleteNumbers(int count)
+        {
+            Random r = new Random();
+
+            do
+            {
+
+                int i = r.Next(0, 9);
+                int j = r.Next(0, 9);
+                if (this.Fields[i, j].number != 0)
+                {
+                    this.Fields[i, j].number = 0;
+                    count--;
+                }
+            } while (count != 0);
+        }
+
+        public void completeTask()
+        {
+            Random r = new Random();
+            this.Completed = this.Fields;
+            bool f = true;
+            for (int i = 0; i < this.Completed.GetLength(0) && f; i++)
+            {
+                for (int j = 0; j < this.Completed.GetLength(1) && f; j++)
+                {
+                    if (this.Completed[i,j].number == 0)
+                    {
+                        int x;
+                        int count = 0;
+                        do
+                        {
+                            x = r.Next(1, 10);
+                            count++;
+                        } while (check(i, j, x, this.Completed) && count < 100);
+                        if (count < 100)
+                        {
+                            this.Completed[i, j].number = x;
+                        }
+                        else f = false;
+                    }
+                }
+            }
+        }
+
+        private void BtnGenerateFull_Click(object sender, RoutedEventArgs e)
+        {
+            do
+            {
+                this.refresh();
+                this.generateTable();
+            } while (!this.isValid(this.Fields));
+            this.writeOut();
+        }
+
+        private void BtnGenerateTask_Click(object sender, RoutedEventArgs e)
+        {
+            do
+            {
+                this.refresh();
+                this.generateTable();
+            } while (!this.isValid(this.Fields));
+            this.deleteNumbers(20);
+            this.writeOut();
+
+        }
+
+        private void BtnTaskComplete_Click(object sender, RoutedEventArgs e)
+        {
+            do
+            {
+                this.completeTask();
+            } while (!this.isValid(this.Completed));
+            this.Fields = this.Completed;
+            this.writeOut();
         }
     }
 }
