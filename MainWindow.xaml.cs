419949afea34f198a7cba1576109f34e58c06a38
karcag.tamas <karcagtamas@gmail.com>
2019-02-20 12:06:25 +0100

    genereate complete table

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,10 +26,51 @@ namespace Sudoku
     public partial class MainWindow : Window
     {
         public Field[,] Fields;
+        public Brush[] colors = new Brush[] { Brushes.DarkMagenta, Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.Cyan, Brushes.Magenta, Brushes.Black, Brushes.DarkGreen, Brushes.Orange };
         public MainWindow()
         {
             InitializeComponent();
             this.Fields = new Field[9, 9];
+            
+
+            do
+            {
+                this.refresh();
+                this.generateTable();
+            } while (!this.isValid());
+                this.writeOut();
+
+        }
+
+        public void refresh()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    this.Fields[i, j] = new Field();
+                }
+            }
+        }
+
+        public void writeOut()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Label label = new Label();
+                    label.Content = this.Fields[i, j].number;
+                    label.FontSize = 16;
+                    label.HorizontalAlignment = HorizontalAlignment.Center;
+                    label.VerticalAlignment = VerticalAlignment.Center;
+                    if (this.Fields[i, j].number != 0) label.Foreground = this.colors[this.Fields[i, j].number - 1];
+                    else label.Background = Brushes.Red;
+                    label.SetValue(Grid.RowProperty, i);
+                    label.SetValue(Grid.ColumnProperty, j);
+                    GridSudoku.Children.Add(label);
+                }
+            }
         }
 
 
@@ -41,31 +82,33 @@ namespace Sudoku
                 for (int j = 0; j < this.Fields.GetLength(1); j++)
                 {
                     int x;
+                    int count = 0;
                     do
                     {
                         x = r.Next(1,10);
-                    } while (check(i,j,x));
-                    Field[i][j].number = x;
+                        count++;
+                    } while (check(i,j,x) && count < 100);
+                    if (count < 100) this.Fields[i,j].number = x;
                 }
             }
         }
 
         public bool check(int i, int j, int x)
         {
-            if (checkRow(j, x) && checkCol(i, x) && checkOneBlock(i,j,x)) return false;
+            if (checkRow(i, x) && checkCol(j, x) && checkOneBlock(i,j,x)) return false;
             else return true;
         }
 
-        public bool checkRow(int j, int x){
-            for (int i = 0; i < this.Fields.GetLength(0); i++){
-                if (Fields[i][j].number === x) return false;
+        public bool checkRow(int i, int x){
+            for (int j = 0; j < this.Fields.GetLength(0); j++){
+                if (this.Fields[i,j].number == x) return false;
             }
             return true;
         }
 
-        public bool checkCol(int i, int x){
-            for (int j = 0; j < this.Fields.GetLength(1); j++){
-                if (Fields[i][j].number === x) return false;
+        public bool checkCol(int j, int x){
+            for (int i = 0; i < this.Fields.GetLength(1); i++){
+                if (this.Fields[i,j].number == x) return false;
             }
             return true;
         }
@@ -75,15 +118,24 @@ namespace Sudoku
             int col = j / 3;
             int posi = row * 3 + 1;
             int posj = col * 3 + 1;
-            if (this.Fields[posi][posj].number === x) return false;
-            if (this.Fields[posi][posj + 1].number === x) return false;
-            if (this.Fields[posi][posj - 1].number === x) return false;
-            if (this.Fields[posi + 1][posj].number === x) return false;
-            if (this.Fields[posi + 1][posj + 1].number === x) return false;
-            if (this.Fields[posi + 1][posj - 1].number === x) return false;
-            if (this.Fields[posi - 1][posj].number === x) return false;
-            if (this.Fields[posi - 1][posj + 1].number === x) return false;
-            if (this.Fields[posi - 1][posj - 1].number === x) return false;
+            if (this.Fields[posi,posj].number == x) return false;
+            if (this.Fields[posi,posj + 1].number == x) return false;
+            if (this.Fields[posi,posj - 1].number == x) return false;
+            if (this.Fields[posi + 1,posj].number == x) return false;
+            if (this.Fields[posi + 1,posj + 1].number == x) return false;
+            if (this.Fields[posi + 1,posj - 1].number == x) return false;
+            if (this.Fields[posi - 1,posj].number == x) return false;
+            if (this.Fields[posi - 1,posj + 1].number == x) return false;
+            if (this.Fields[posi - 1,posj - 1].number == x) return false;
+            return true;
+        }
+
+        public bool isValid()
+        {
+            foreach (var i in this.Fields)
+            {
+                if (i.number == 0) return false;
+            }
             return true;
         }
     }
